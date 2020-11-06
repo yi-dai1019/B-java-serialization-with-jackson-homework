c7f77c9fb3356e4d6acf632eeae660ada9423d66
yi-dai <yi.dai@thoughtworks.com>
2020-11-06 09:14:14 +0800

    finish problem1

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,6 +1,10 @@
 package com.thoughtworks.capability.gtb.vo;
 
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +17,8 @@ public class EventVo {
   private String id;
   private String name;
   private EventType type;
+  @JsonFormat(shape = JsonFormat.Shape.NUMBER)
   private Date time;
+  @JsonUnwrapped
   private UserVo user;
 }
